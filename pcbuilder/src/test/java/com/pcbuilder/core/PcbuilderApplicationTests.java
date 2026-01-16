3551093f0e41ce984d3e7c0ae1ccfe147f8c262f
Alysseum17 <ultrasarsenal2006@gmail.com>
2026-01-16 02:27:42 +0200

    Start init

---
+++ pcbuilder/src/test/java/com/pcbuilder/core/PcbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcbuilder.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
